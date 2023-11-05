7fd06d4d3f34e7fc8c2a91acd70435deb75d2975
andrescuello7 <andrescuellotrabajo@gmail.com>
2023-11-04 21:01:28 -0300

    add implement profile

--- src/app/about/components/about.tsx
+++ src/app/about/components/about.tsx
@@ -1,4 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
+"use client"
+
+import Link from "next/link";
 import styles from "./about.module.css";
 
 export default function AboutUs() {
@@ -15,8 +18,12 @@ export default function AboutUs() {
           </div>
         </div>
         <div>
-          <button className={styles.button}>Start</button>
-          <button className={styles.button_outline}>Get a Demo</button>
+          <Link href={`/login`}>
+            <button className={styles.button}>Start</button>
+          </Link>
+          <Link href={`/register`}>
+            <button className={styles.button_outline}>Get a Demo</button>
+          </Link>
         </div>
       </div>
     </>
