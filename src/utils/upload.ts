4aac1747041e93e5de033406a275882d763febe9
andrescuello7 <andrescuellotrabajo@gmail.com>
2023-11-05 12:49:01 -0300

    envirement options

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -6,8 +6,8 @@ export default function UploadMethod() {
             const pic = e.target.files[0];
             const formData = new FormData();
             formData.append("file", pic);
-            formData.append("upload_preset", "wkuf5yo4");
-            const { data } = await axios.post("https://api.cloudinary.com/v1_1/five-drive/upload", formData);
+            formData.append(`${process.env.NEXT_PUBLIC_API_CLOUDINARY_TOKEN_KEY}`, `${process.env.NEXT_PUBLIC_API_CLOUDINARY_TOKEN}`);
+            const { data } = await axios.post(`${process.env.NEXT_PUBLIC_API_CLOUDINARY}`, formData);
             return data.url;
         } catch (error) {
             console.log({ error });
